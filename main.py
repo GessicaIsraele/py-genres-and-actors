bfbbc502ba6372814458dffdf93391ba212c8455
Géssica Israele <gessicaisraele@hotmail.com>
2026-02-03 19:11:35 -0300

    Implemented Genre and Actor models and main logic

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    pass
+    # 1. CREATE
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    dramma = Genre.objects.create(name="Dramma")
+
+    george = Actor.objects.create(first_name="George", last_name="Klooney")
+    kianu = Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+
+    # 2. UPDATE
+    dramma.name = "Drama"
+    dramma.save()
+
+    george.last_name = "Clooney"
+    george.save()
+
+    kianu.first_name = "Keanu"
+    kianu.last_name = "Reeves"
+    kianu.save()
+
+    # 3. DELETE
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    # 4. RETURN
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
